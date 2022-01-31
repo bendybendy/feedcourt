fd6f6b9342139a508d7cc9eb3f2fd83ad7d1029b
Benjamin Kahn <bkahn@redhat.com>
2022-01-31 14:26:30 -0500

    Add caching to handle feeds that work sometimes

---
+++ cachefeeds.py
@@ -0,0 +1,69 @@
+"""
+    The goal of this script is to read through the list of feeds
+    then pull the latest version. If it is available and updated, 
+    cache the feed in a known local path for the parsing script
+    to read.
+
+    FUTURE: Record in the database each time the cache is updated for a feed and 
+    failures
+
+    FUTURE: Process links in the database for future analysis/search engine
+"""
+import requests
+import feedparser
+
+import re
+import html
+from bs4 import BeautifulSoup
+
+import argparse
+import pathlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output', help="Specify the output folder", type=pathlib.Path, default=pathlib.Path('.'))
+parser.add_argument('-f', '--feedlist', help="The text file of feeds to load", type=argparse.FileType('r'), default="feedlist.txt")
+parser.add_argument('-v', '--verbose', help="Be more verbose", action="store_true")
+args = parser.parse_args()
+
+feedlist = args.feedlist
+
+pattern = re.compile('[\W_]+')
+noquote = re.compile("[\"\']")
+
+ACCEPT_HEADER = "application/atom+xml,application/rdf+xml,application/rss+xml,application/x-netcdf,application/xml;q=0.9,text/xml;q=0.2,*/*;q=0.1"
+USER_AGENT = "UniversalFeedParser/%s +http://feedparser.org/" % feedparser.__version__
+
+
+for url in feedlist:
+    cachefile = str(args.output.resolve()) + '/' + pattern.sub('', url) + ".rss"
+    if args.verbose:
+        print (url.strip())
+        print ("  " + cachefile)
+
+    r = requests.get(url.strip(), headers={'user-agent': USER_AGENT, 'accept': ACCEPT_HEADER})
+
+    # We only care if the feed is available
+    if r.status_code == 200:
+        # And can be parsed correcty
+        f = feedparser.parse(r.text)
+
+        if args.verbose:
+            print ("  " + str(f.bozo))
+            if f.bozo:
+                print ("  " + "Warning or error found! Will still update")
+                print ("    " + f.bozo_exception.getMessage())
+                print ("    " + str(f.bozo_exception.getLineNumber()))
+                print ("    " + r.text)
+
+        if "title" in f['feed'] and f['feed']['title']:
+            site = f['feed']['title']
+        else:
+            site = f['feed']
+
+        if isinstance(site, str):
+            with open(cachefile, 'w') as o:
+                o.write(r.text)
+        elif args.verbose:
+            print ("  Feed didn't have a title, likely an error (no update)")
+        else:
+            print (url.strip() + " : bad update")
--- parsefeeds.py
+++ parsefeeds.py
@@ -63,6 +63,8 @@ def get_entry_metadata (e):
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output', help="Specify the output folder", type=pathlib.Path, default=pathlib.Path('.'))
 parser.add_argument('-f', '--feedlist', help="The text file of feeds to load", type=argparse.FileType('r'), default="feedlist.txt")
+parser.add_argument('-i', '--input', help="The folder of cached RSS feeds to use", type=pathlib.Path, default=pathlib.Path('.'))
+parser.add_argument('-c', '--cached', help="Use a cached directory of RSS feeds instead", type="store_true")
 parser.add_argument('-v', '--verbose', help="Be more verbose", action="store_true")
 args = parser.parse_args()
 
@@ -102,7 +104,12 @@ output +="<span class='sorter'><a href='./jumble.html'> jumble </a> </span></p><
 joutput +="<span class='sorter'><a href='./'> sort </a></span> </p></div> <div id='jumblewrapper'>" 
 
 for url in feedlist:
-    f = feedparser.parse(url)
+    if args.cache:
+        cachefile = str(args.output.resolve()) + '/' + pattern.sub('', url) + ".rss"
+        f = feedparser.parse(cachefile)
+    else:
+        f = feedparser.parse(url)
+        
     if "title" in f['feed'] and f['feed']['title']:
         site = f['feed']['title']
     else:
