0380decfedc2b1729a9bd6cbb3080979beccaa44
Benjamin Kahn <bkahn@redhat.com>
2022-01-31 15:27:48 -0500

    More verbose

--- parsefeeds.py
+++ parsefeeds.py
@@ -106,6 +106,8 @@ joutput +="<span class='sorter'><a href='./'> sort </a></span> </p></div> <div i
 for url in feedlist:
     if args.cached:
         cachefile = str(args.output.resolve()) + '/' + pattern.sub('', url.strip()) + ".rss"
+        if args.verbose:
+            print (cachefile)
         f = feedparser.parse(cachefile)
     else:
         f = feedparser.parse(url)
